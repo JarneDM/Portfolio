a9c4151c695d80c0fda6cbf10a2ff7206c27c2a9
JarneDM <jarne.demeyer05@hotmail.com>
2025-09-24 22:56:46 +0200

    working on about page, gotta look for changing routing projects, little change topbar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
+import filesData from "../data/files.json";
 
 function About() {
+  const location = useLocation();
+  const content = filesData.find((file) => file.route === location.pathname);
+
+  if (!content) return <h1>Not found</h1>;
+
   return (
-    <div>
-      <div className="bg-[#FFFFFF]"></div>
+    <div className="p-10 space-y-4 min-w-[20rem]">
+      <h1 className="text-5xl mb-10 border-b-[2px] border-b-white py-1 w-min">{content.title}</h1>
+      <div className="shadow-sm shadow-blue-400 space-y-4 rounded-md p-2 w-min min-w-[20rem] text-justify">
+        {content.description.map((description, idx) => (
+          <p className="lg:w-[40rem] md:w-[30rem] sm:w-[20rem]" key={idx}>
+            {description}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,10 +9,15 @@ function Home() {
     <div className="w-full flex justify-center items-center">
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-10 p-4 flex-1 overflow-auto">
         {filesData.map((file) => (
-          <Link key={file.id} to={file.route} className="flex flex-col justify-center items-center p-2 w-[10rem] cursor-pointer">
-            <img className="h-12 mb-2 filter invert brightness-0" src={fileIcon} alt={file.title} />
-            <p>{file.title}.txt</p>
-          </Link>
+          <>
+            <div key={file.id} className="hidden">
+              <TopBar file={file} />
+            </div>
+            <Link key={file.id} to={file.route} className="flex flex-col justify-center items-center p-2 w-[10rem] cursor-pointer">
+              <img className="h-12 mb-2 filter invert brightness-0" src={fileIcon} alt={file.title} />
+              <p>{file.title}.txt</p>
+            </Link>
+          </>
         ))}
       </div>
     </div>
