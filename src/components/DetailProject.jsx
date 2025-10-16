6dc23019f4fdd282df8e3e490c442074840e3b4a
JarneDM <jarne.demeyer05@hotmail.com>
2025-10-17 00:54:30 +0200

    add links to projects to visit

--- src/components/DetailProject.jsx
+++ src/components/DetailProject.jsx
@@ -12,6 +12,29 @@ function DetailProject() {
       <h1 className="text-3xl font-bold text-white border-b-2 border-blue-500 pb-2">{project.title}</h1>
 
       <p className="text-gray-200 text-lg leading-relaxed">{project.description}</p>
+
+      <div className="flex space-x-4">
+        {project.liveDemo && (
+          <a
+            href={project.liveDemo}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-black bg-white hover:bg-blue-500 hover:text-white px-2 py-1 rounded transition-colors duration-300"
+          >
+            Live Demo
+          </a>
+        )}
+        {project.sourceCode && (
+          <a
+            href={project.sourceCode}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-black bg-white hover:bg-blue-500 hover:text-white px-2 py-1 rounded transition-colors duration-300"
+          >
+            Source Code
+          </a>
+        )}
+      </div>
     </div>
   );
 }
