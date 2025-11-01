284bc288f31d926bd87ca26f65d58aecae0f4730
JarneDM <jarne.demeyer05@hotmail.com>
2025-11-01 21:34:49 +0100

    little change to styling detail project page

--- src/components/DetailProject.jsx
+++ src/components/DetailProject.jsx
@@ -8,7 +8,7 @@ function DetailProject() {
   if (!project) return <h1>Not found</h1>;
 
   return (
-    <div className="p-8 md:max-h-[38rem] overflow-y-auto custom-scrollbar max-w-3xl mx-auto bg-white/10 backdrop-blur-md rounded-2xl shadow-lg space-y-6">
+    <div className="p-8 md:max-h-[38rem] lg:max-h-[45rem] overflow-y-auto custom-scrollbar max-w-3xl mx-auto bg-white/10 backdrop-blur-md rounded-2xl shadow-lg space-y-6">
       <h1 className="text-3xl font-bold text-white border-b-2 border-blue-500 pb-2">{project.title}</h1>
 
       {project.image && (
@@ -31,28 +31,30 @@ function DetailProject() {
 
       <p className="text-gray-200 text-lg leading-relaxed">{project.description}</p>
 
-      <div className="flex space-x-4">
-        {project.liveDemo && (
-          <a
-            href={project.liveDemo}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-black bg-white hover:bg-blue-500 hover:text-white px-2 py-1 rounded transition-colors duration-300"
-          >
-            Live Demo
-          </a>
-        )}
-        {project.sourceCode && (
-          <a
-            href={project.sourceCode}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-black bg-white hover:bg-blue-500 hover:text-white px-2 py-1 rounded transition-colors duration-300"
-          >
-            Source Code
-          </a>
-        )}
-      </div>
+      {project.liveDemo || project.sourceCode ? (
+        <div className="flex space-x-4">
+          {project.liveDemo && (
+            <a
+              href={project.liveDemo}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black bg-white hover:bg-blue-500 hover:text-white px-2 py-1 rounded transition-colors duration-300"
+            >
+              Live Demo
+            </a>
+          )}
+          {project.sourceCode && (
+            <a
+              href={project.sourceCode}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black bg-white hover:bg-blue-500 hover:text-white px-2 py-1 rounded transition-colors duration-300"
+            >
+              Source Code
+            </a>
+          )}
+        </div>
+      ) : null}
     </div>
   );
 }
